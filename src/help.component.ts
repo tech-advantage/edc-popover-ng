1c0b8340582f327db09132fce3514c5b735b8f0d
mayer <Daniel.MAYER@tech-advantage.com>
2017-03-15 17:34:26 +0100

    Change focus handling : now in HelpComponent  + add dark style option

--- src/help.component.ts
+++ src/help.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, HostListener } from '@angular/core';
 import { HelpService } from './help.service';
 import { Helper, Link } from 'edc-web-publishing-js';
+import { NgbPopover } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'edc-help',
   templateUrl: './help.component.html',
   styleUrls: ['help.less']
 })
-
 export class HelpComponent implements OnInit {
 
   helper: Helper;
+  @ViewChild('popover') popover: NgbPopover; // get the popover element by its name declared in the component template
 
   @Input() key: string;
   @Input() subKey: string;
   @Input() placement: string;
+  @Input() dark: boolean;
+
+  // for closing popover when focus los
+  @HostListener('document:click')
+  onDocumentClick() {
+    this.popover.close();
+  }
 
   constructor(
     private helpService: HelpService
