b2a3d8b278854dea725057f76590fd1e18dae02f
Renaud Aste <renaud.aste@tech-advantage.com>
2017-04-14 16:49:41 +0200

    More precise import.

--- src/help.module.ts
+++ src/help.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelpComponent } from './help.component';
 import { HelpService } from './help.service';
-import { PopoverModule } from 'ngx-bootstrap';
+import { PopoverModule } from 'ngx-bootstrap/popover';
 
 @NgModule({
   imports: [
