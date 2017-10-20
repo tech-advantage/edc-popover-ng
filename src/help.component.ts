c1c61d3cb0b6e6558db3c98a97c0e48f0b4b4bc3
Renaud Aste <renaud.aste@zenika.com>
2017-10-20 12:59:12 +0200

    [EDC-958] Rename "Links" label in popover. Story:[EDC-750]
    Change-Id: Ia8bb4462b38615ee48a24bc0c347e2262b76d449

--- src/help.component.ts
+++ src/help.component.ts
@@ -13,7 +13,7 @@ import { HelpConstants } from './help.constants';
         <article class="popover-article">{{ helper?.description }}</article>
         <div class="see-also">
           <div *ngIf="helper?.articles.length">
-            <h6><strong><span> Need more...</span></strong></h6>
+            <h6><strong><span>Need more...</span></strong></h6>
             <ul class="list-unstyled see-also-list">
               <li *ngFor="let article of helper.articles; let key = index" class="see-also-item" (click)="goToArticle(key)">
                 <div class="article-link">-{{article.label}}</div>
@@ -21,7 +21,7 @@ import { HelpConstants } from './help.constants';
             </ul>
           </div>
           <div *ngIf="helper?.links.length">
-            <h6><strong><span>Links</span></strong></h6>
+            <h6><strong><span>Related Topics</span></strong></h6>
             <ul class="list-unstyled see-also-list">
               <li *ngFor="let link of helper.links" class="see-also-item" (click)="goToLink(link)">
                 <div class="article-link">-{{link.label}}</div>
