dd717325ca3e7cf635ae7b20c8f32c317a000216
Daniel Mayer <Daniel.MAYER@tech-advantage.com>
2019-07-29 15:36:25 +0200

    [EDC-2313] Clean dependencies
    - set version to 3.0.0-beta-0

--- projects/edc-popover-ng/src/lib/utils/test-helpers.ts
+++ projects/edc-popover-ng/src/lib/utils/test-helpers.ts
@@ -1,14 +1,13 @@
-import { forEach, assign } from 'lodash';
 import { Provider } from '@angular/core';
 
-export function mockService(provide: any, methods?: string[]): Provider {
+export function mockService(provide: any, methods: string[] = []): Provider {
   class MockService {}
-  forEach(methods, method => MockService.prototype[method] = () => {});
+  methods.forEach(method => MockService.prototype[method] = () => {});
   return {provide: provide, useClass: MockService};
 }
 
 export function mock<T>(type: { new(... args: any[]): T; }, objet: any = {}): T {
   const entity: T = new type();
-  assign(entity, objet);
+  Object.assign(entity, objet);
   return entity;
 }
